5f37194c54c53f8f5b770e39753050147d4df9b2
David <krpec@sovanet.cz>
2021-03-21 18:09:44 +0100

    load book list on the search page if it is not loaded

--- src/components/pages/SearchPage/index.tsx
+++ src/components/pages/SearchPage/index.tsx
@@ -3,11 +3,13 @@ import { search } from '../../../api/Books'
 import { Book } from '../../../api/types'
 import SearchPage from './SearchPage'
 import {toast} from 'react-toastify'
+import useBookList from '../../../hooks/useBookList'
 
 function SearchPageContainer() {
   const [searchTerm, setSearchTerm] = useState<string>('')
   const [isSearching, setIsSearching] = useState<boolean>(false)
   const [results, setResults] = useState<Book[] | undefined | null>(null)
+  const { loadBookList, isBookListLoaded } = useBookList()
 
   useEffect(() => {
     if (searchTerm) {
@@ -29,6 +31,17 @@ function SearchPageContainer() {
       setResults(null)
     }
   }, [searchTerm])
+
+  /**
+   * The search results doesn't know to which shelf they belong.
+   * So we have to make sure that the bookList is Loaded.
+   * In case user will access directly the /search route.
+   */
+  useEffect(() => {
+    if (!isBookListLoaded) {
+      loadBookList()
+    }
+  }, [loadBookList, isBookListLoaded])
 
   return (
     <SearchPage
